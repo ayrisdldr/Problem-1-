ea0e9f227205b0ef45991c312bb83b322be3e74d
ayrisdldr <95179874+ayrisdldr@users.noreply.github.com>
2021-12-26 23:08:31 +0300

    Add files via upload

---
+++ Problem 1 (Temperature).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Problem Set 2
+# Name: Ayris
+# Time Spent : 7 minutes
+
+
+# In[1]:
+
+
+celsius = int(input("Enter the temperature in Degrees Celsius that you would like to convert: "))
+fahrenheit = (celsius * 9/5) + 32
+print("The converted temperature is", fahrenheit)
+
